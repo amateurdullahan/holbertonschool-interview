a468a1b3ad9a958edfad0334273e3a91dec1bcfd
Dylan Carey <dylan.carey@live.com>
2021-04-22 16:49:47 -0500

    task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    keys = [0]
+    last = 0
+
+    while last < len(keys):
+        last = len(keys)
+        for box in range(len(boxes)):
+            if box in keys:
+                for key in boxes[box]:
+                    if key < len(boxes) and key not in keys:
+                        keys.append(key)
+
+    return len(keys) == len(boxes)

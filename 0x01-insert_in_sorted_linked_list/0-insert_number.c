09e3850eb200e2764b02c6c6c7262c092441fa61
Dylan Carey <dylan.carey@live.com>
2021-04-29 23:45:08 -0500

    task finished

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+  * insert_node - insert node L)
+  * @head: head of list
+  * @number: number to insert
+  *
+  * Return: linked list
+  */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode;
+	listint_t *current;
+
+	current = *head;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->n = number;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+	}
+	else if (current->n > number)
+	{
+		newNode->next = *head;
+		*head = newNode;
+	}
+	else
+	{
+		while (current->next != NULL && current->next->n <= number)
+		{
+			current = current->next;
+		}
+		newNode->next = current->next;
+		current->next = newNode;
+	}
+
+	return (newNode);
+}

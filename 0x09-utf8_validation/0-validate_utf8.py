274238f7c4d0b4bb0c5da93d5280fe873c1142bc
Dylan Carey <dylan.carey@live.com>
2021-06-24 21:35:39 -0500

    interview task

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Validate da UTF8 """
+
+
+def validUTF8(data):
+
+    if type(data) is not list:
+        return False
+
+    byteList = []
+
+    for num in data:
+        try:
+            byteList.append(num.to_bytes(1, 'big'))
+        except OverflowError:
+            byteList.append(num.to_bytes(2, 'big'))
+    test = b''.join(byteList)
+
+    try:
+        test.decode('utf-8')
+    except UnicodeDecodeError:
+        return False
+
+    return True

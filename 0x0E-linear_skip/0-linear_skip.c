be2ef58b4b604006a658ff040ee012de1caf2358
Dylan Carey <dylan.carey@live.com>
2021-08-05 21:10:00 -0500

    int prep

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,42 @@
+#include "search.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *tor = list;
+	skiplist_t *har = list;
+	int last;
+
+	if (list == NULL)
+		return (NULL);
+	har = har->express;
+	while (har && har->n < value)
+	{
+		if (har == NULL)
+			break;
+		printf("Value checked at index [%d] = [%d]\n", (int)har->index, har->n);
+		tor = hare;
+	    har = har->express;
+	}
+	if (har != NULL)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)har->index, har->n);
+		last = (int)har->index;
+	}
+	else
+	{
+		har = tor;
+		while (har->next != NULL)
+			har = har->next;
+		last = (int)har->index;
+	}
+	printf("Value found between indexes [%d] and [%d]\n", (int)tor->index, last);
+	har = tor->express;
+	while (tor != NULL && tor != har)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)tor->index, tor->n);
+		if (tor->n == value)
+			return (tor);
+		tor = tor->next;
+	}
+	return (NULL);
+}

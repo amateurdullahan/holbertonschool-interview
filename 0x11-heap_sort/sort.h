5c49985f69220b926a4c4c43e04017f8563f9a23
amateurdullahan <56708330+amateurdullahan@users.noreply.github.com>
2021-10-13 14:01:44 -0500

    Delete 0x11-heap_sort directory

--- 0x11-heap_sort/sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-
-void print_array(const int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void heapify(int *arr, size_t size, size_t root);
-void swap(int *arr, int *parent, int *child);
-
-#endif /* SORT_H */

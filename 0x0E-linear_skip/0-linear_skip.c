28d9e5c30a937ce8f42aa9af1f61cf1e3bdbe39c
Dylan Carey <dylan.carey@live.com>
2021-11-04 19:01:30 -0500

    betty fixes

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,5 +1,13 @@
 #include "search.h"
 
+/**
+  * linear_skip - search thru skip list
+  * @list: list to search
+  * @value: value searching for
+  *
+  * Return: pointer to node or null
+  */
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *tor = list;

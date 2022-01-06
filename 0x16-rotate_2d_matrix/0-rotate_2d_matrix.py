d31d1f0d45afe1204a78de524fc42acf421ad151
Dylan Carey <dylan.carey@live.com>
2022-01-06 13:33:17 -0600

    first push

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Enter The Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotates da matrix"""
+    size = len(matrix) - 1
+    for i in range(size):
+        for j in range(i, size - i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[size - j][i]
+            matrix[size - j][i] = matrix[size - i][size - j]
+            matrix[size - i][size - j] = matrix[j][size - i]
+            matrix[j][size - i] = temp

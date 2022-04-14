c131b66592198aa92287ed693986925be9543c01
Dylan Carey <dylan.carey@live.com>
2022-04-14 15:07:35 -0500

    Pascal is a pal

---
+++ 0x1F-pascal_triangle.py/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains function to display Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """returns list of lists representing Pascal's Triangle"""
+    output = []
+    for b in range(n):
+        row = [1]
+        for a in range(b):
+            if a + 1 == b:
+                res = 1
+            else:
+                res = output[b - 1][a] + output[b - 1][a + 1]
+            row.append(res)
+        output.append(row)
+    return output
+    
